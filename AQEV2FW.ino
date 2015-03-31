efd4af5ffd0d85f6387357e3f15dc1e8e1d19711
Victor Aprea <victor.aprea@wickeddevice.com>
2015-03-31 13:38:46 -0400

    added library includes and startup Serial banner

--- AQEV2FW.ino
+++ AQEV2FW.ino
@@ -1,5 +1,29 @@
-void setup(){
+#include <Wire.h>
+#include <SPI.h>
+#include <WildFire.h>
+#include <WildFire_CC3000.h>
+#include <WildFire_PubSubClient.h>
+#include <TinyWatchdog.h>
+#include <MCP342x.h>
+#include <LMP91000.h>
 
+#define AQEV2FW_VERSION "0.1"
+
+WildFire wf;
+WildFire_CC3000 cc3000;
+TinyWatchdog tinywdt;
+LMP91000 lmp91000;
+MCP342x mcp342x;
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println(F(" +------------------------------------+"));
+  Serial.println(F(" |   Welcome to Air Quality Egg 2.0   |"));
+  Serial.println(F(" |        Firmware Version " AQEV2FW_VERSION "        |"));
+  Serial.println(F(" +------------------------------------+"));  
+  Serial.println();
+  
+  
 }
 
 void loop(){
