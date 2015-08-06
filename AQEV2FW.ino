8dac09795d9c5288e3055017f99ef31587df63fe
Victor Aprea <victor.aprea@wickeddevice.com>
2015-08-06 10:42:26 -0400

    re-implemented/fixed scaling temperature compensation and implemented baseline offset temperature compensation. Addresses issue #9

--- AQEV2FW.ino
+++ AQEV2FW.ino
@@ -4534,25 +4534,61 @@ void no2_convert_from_volts_to_ppb(float volts, float * converted_value, float *
     first_access = false;
   }
   
-  // piecewise temperature coefficient of span for NO2
-  // -20C to 20C  0.1%/degC 
-  // 20C to 50C   0.4%/degC
-  if(temperature_degc < 20.0f){
-    temperature_coefficient_of_span = 0.001f; // 0.1%
+  // apply piecewise linear regressions
+  // to signal scaling effect curve
+  float scaling_slope = 0.0f;
+  float scaling_intercept = 0.0f;  
+  if(temperature_degc < 0.0f){                 // < 0C  
+    scaling_slope = -0.0355739076f;
+    scaling_intercept = 97.9865525718f;
   }
-  else{
-    temperature_coefficient_of_span = 0.004f; // 0.4%    
+  else if(temperature_degc < 20.0f){           // 0C .. 20C   
+    scaling_slope = 0.1702484721f;
+    scaling_intercept = 97.9953985672f; 
   }
+  else{                                        // > 20C   
+    scaling_slope = 0.3385634354f;
+    scaling_intercept = 94.6638669473f;
+  }
+  float signal_scaling_factor_at_temperature = ((scaling_slope * temperature_degc) + scaling_intercept)/100.0f;
+  // divide by 100 becauset the slope/intercept graphs have scaling factors in value
+
+  // apply piecewise linear regressions
+  // to baseline offset effect curve
+  float baseline_offset_ppb_slope = 0.0f;
+  float baseline_offset_ppb_intercept = 0.0f;
+                                                                     
+  if(temperature_degc < 33.0f){                          // < 33C
+    baseline_offset_ppb_slope = -0.0007019288f;
+    baseline_offset_ppb_intercept = 0.0177058403f;
+  }
+  else if(temperature_degc < 38.0f){                     // 33C .. 38C
+    baseline_offset_ppb_slope = -0.0085978946f;
+    baseline_offset_ppb_intercept = 0.2777254052f;
+  }
+  else if(temperature_degc < 42.0f){                     // 38C .. 42C
+    baseline_offset_ppb_slope = -0.0196092331f;
+    baseline_offset_ppb_intercept = 0.6994563331f;    
+  }
+  else if(temperature_degc < 46.0f){                     // 42C .. 46C
+    baseline_offset_ppb_slope = -0.0351416006f;
+    baseline_offset_ppb_intercept = 1.3566041809f;          
+  }
+  else{                                                  // > 46C
+    baseline_offset_ppb_slope = -0.0531894279f;
+    baseline_offset_ppb_intercept =  2.1948987152f;        
+  }  
+  float baseline_offset_ppm_at_temperature = ((baseline_offset_ppb_slope * temperature_degc) + baseline_offset_ppb_intercept); 
+  float baseline_offset_ppb_at_temperature = baseline_offset_ppm_at_temperature * 1000.0f;
+  // multiply by 1000 because baseline offset graph shows NO2 in ppm  
+  float baseline_offset_voltage_at_temperature = baseline_offset_ppb_at_temperature / no2_slope_ppb_per_volt;
   
-  temperature_compensated_slope = no2_slope_ppb_per_volt;
-  temperature_compensated_slope /= (1.0f + (temperature_coefficient_of_span * (20 - temperature_degc)));
-  
-  *converted_value = (volts - no2_zero_volts) * no2_slope_ppb_per_volt;
+  *converted_value = (volts - no2_zero_volts) * -1.0f * no2_slope_ppb_per_volt;
   if(*converted_value <= 0.0f){
     *converted_value = 0.0f; 
   }
   
-  *temperature_compensated_value = (volts - no2_zero_volts) * temperature_compensated_slope;
+  *temperature_compensated_value = (volts - no2_zero_volts - baseline_offset_voltage_at_temperature) * -1.0f * no2_slope_ppb_per_volt / signal_scaling_factor_at_temperature;
   if(*temperature_compensated_value <= 0.0f){
     *temperature_compensated_value = 0.0f;
   }
@@ -4600,29 +4636,58 @@ void co_convert_from_volts_to_ppm(float volts, float * converted_value, float * 
     first_access = false;
   }
 
-  // piecewise temperature coefficient of span for NO2
-  // -20C to 20C  0.6%/degC
-  // 20C to 50C   0.4%/degC
-  if(temperature_degc < 20.0f){
-    temperature_coefficient_of_span = 0.006f; // 0.1%
+  // apply piecewise linear regressions
+  // to signal scaling effect curve
+  float scaling_slope = 0.0f;
+  float scaling_intercept = 0.0f;  
+  if(temperature_degc < 0.0f){       // < 0C
+    scaling_slope = 0.926586438f;
+    scaling_intercept = 88.2942019565f;
   }
-  else{
-    temperature_coefficient_of_span = 0.004f; // 0.4%    
+  else if(temperature_degc < 20.0f){ // 0C .. 20C
+    scaling_slope = 0.6072408915f;
+    scaling_intercept = 87.9176593244f; 
   }
+  else{                              // > 20C
+    scaling_slope = 0.2600853674f;
+    scaling_intercept = 95.6168149016f;
+  }
+  float signal_scaling_factor_at_temperature = ((scaling_slope * temperature_degc) + scaling_intercept)/100.0f;
+  // divide by 100 becauset the slope/intercept graphs have scaling factors in value
   
-  temperature_compensated_slope = co_slope_ppm_per_volt;
-  temperature_compensated_slope /= (1.0f + (temperature_coefficient_of_span * (20 - temperature_degc)));
-  
-  
+  // apply piecewise linear regressions
+  // to baseline offset effect curve
+  float baseline_offset_ppm_slope = 0.0f;
+  float baseline_offset_ppm_intercept = 0.0f;
+                
+  if(temperature_degc < 15.5f){                          // no correction for < 15.5C
+    baseline_offset_ppm_slope = 0.0f;
+    baseline_offset_ppm_intercept = 0.0f;
+  }
+  else if(temperature_degc < 25.0f){                      // 15.5C .. 25C
+    baseline_offset_ppm_slope = 0.2590260005f;
+    baseline_offset_ppm_intercept = -4.0290395187f;
+  }
+  else if(temperature_degc < 32.0f){                      // 25C .. 32C
+    baseline_offset_ppm_slope = 0.5387700048f;
+    baseline_offset_ppm_intercept = -11.0899532317f;
+  }
+  else{                                                   // > 32C
+    baseline_offset_ppm_slope = 0.824964228f;
+    baseline_offset_ppm_intercept = -20.3665881995f;        
+  }  
+  float baseline_offset_ppm_at_temperature = (baseline_offset_ppm_slope * temperature_degc) + baseline_offset_ppm_intercept;  
+  float baseline_offset_voltage_at_temperature = baseline_offset_ppm_at_temperature / co_slope_ppm_per_volt;
+    
   *converted_value = (volts - co_zero_volts) * co_slope_ppm_per_volt;
   if(*converted_value <= 0.0f){
     *converted_value = 0.0f; 
   }
   
-  *temperature_compensated_value = (volts - co_zero_volts) * temperature_compensated_slope;
+  *temperature_compensated_value = (volts - co_zero_volts - baseline_offset_voltage_at_temperature) * co_slope_ppm_per_volt / signal_scaling_factor_at_temperature;
   if(*temperature_compensated_value <= 0.0f){
     *temperature_compensated_value = 0.0f;
-  }  
+  }
 }
 
 boolean publishCO(){
